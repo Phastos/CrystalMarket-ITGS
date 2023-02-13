b74f6ae5a9ebe371b7fa37ddfade991115ef97b2
Phastos <stolosa.exe@gmail.com>
2023-02-13 10:53:04 -0500

    init

--- script.js
+++ script.js
@@ -3,6 +3,6 @@ const nav = document.getElementById ("navbar");
 
 if (bar) {
     bar.addEventListener("click", () => {
-        nav.classList.add ("active")
+        nav.classList.add ("active");
     })
 }
